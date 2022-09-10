092c0ad3b97524ca6b4a6aa8b3efc05fa84df190
“ifeanyichukwujames” <ifeanyichukwu.eze.james@gmail.com>
2022-09-09 18:29:58 -0700

    +- commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - Assigns random number to int n everytime
  *
- * Return:Always return 0
+ * Return:Always 0
  *
  */
 int main(void)
@@ -15,15 +15,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
 	{
-		printf("n%d is negative\n", n);
+		printf("%d is negative\n", n);
 	}
        	else if (n == 0)
 	{
-		printf("n%d is zero\n", n);
+		printf("%d is zero\n", n);
 	}
        	else
 	{
-		printf("n%d is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	return (0);
 }
