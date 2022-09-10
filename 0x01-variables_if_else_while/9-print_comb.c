3b5dd5f8ccc1cbac9fc16cf8e293d5d2a76e04a3
“ifeanyichukwujames” <ifeanyichukwu.eze.james@gmail.com>
2022-09-09 21:55:39 -0700

    9 c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,10 +12,10 @@ int main(void)
 	for (i = 10; i < 20; i++)
 	{
 		putchar((i % 10) + '0');
-		if ((i)! = 19)
+		if ((i) ! = 19)
 		{
 			putchar(',');
-			putchar('');
+			putchar();
 		}
 	}
 	putchar('\n');
