4f9e86bd0a10bec062d15906981542bbcb8694f6
“ifeanyichukwujames” <ifeanyichukwu.eze.james@gmail.com>
2022-09-09 18:05:15 -0700

    +- commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,17 +12,18 @@ int main(void)
 	int n;
 
 	srand(time(0));
-
-	n = rand() - RAND_MAX/2;
+	n = rand() - RAND_MAX / 2;
 	if (n < 0)
 	{
-		printf("%d is negative\n", n);
-	} else if (n == 0)
+		printf("n%d is negative\n", n);
+	}
+       	else if (n == 0)
 	{
-		printf("%d is zero\n", n);
-	} else
+		printf("n%d is zero\n", n);
+	}
+       	else
 	{
-		printf("%d is positive\n", n);
+		printf("n%d is positive\n", n);
 	}
 	return (0);
 }
