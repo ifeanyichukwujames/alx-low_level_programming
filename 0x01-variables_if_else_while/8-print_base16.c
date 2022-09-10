f8dcdae1df2659c609f459fd5505b139e4124149
“ifeanyichukwujames” <ifeanyichukwu.eze.james@gmail.com>
2022-09-09 21:00:32 -0700

    base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	unsigned char a = '0';
-	int i;;
+	int i;
 
 	for (i = 0; i < 10; i++)
 	{
@@ -16,7 +16,7 @@ int main(void)
 		a++;
 	}
 	a = '1';
-	for (i = 0; i< 6; i++)
+	for (i = 0; i < 6; i++)
 	{
 		putchar('0' + a);
 		a++;
