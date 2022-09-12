7302969829afc368e3855ede082ce48e5a164c2a
“ifeanyichukwujames” <ifeanyichukwu.eze.james@gmail.com>
2022-09-12 06:44:09 -0700

    102 task

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
-/** main - Entry point 
+/**
+ * main - Entry point 
  * Description: prints all possible combination of 2-digit numbers
  * Return: Always 0 (success)
  */
 int main(void)
-{	
+{
 	int c, i, k, j;
 	
 	for (c = 48; c <= 57; c++)
