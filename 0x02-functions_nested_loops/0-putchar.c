a0a9bc880422da4aee35273ff3d3402278c4c11d
“ifeanyichukwujames” <ifeanyichukwu.eze.james@gmail.com>
2022-09-13 14:57:21 -0700

    putchar commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0
+ *
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('a');
+	_putchar('\n');
+	return (0);
+}
