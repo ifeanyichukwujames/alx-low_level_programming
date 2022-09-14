b1a54663d3ef4d554470e4a3e24ba566ed95d412
“ifeanyichukwujames” <ifeanyichukwu.eze.james@gmail.com>
2022-09-14 06:12:10 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,11 +2,12 @@
 
 /**
  * prints_times_table - prints times table
- * @n: times table
+ * @n : times table to use
  * Return: void
  */
 
 void print_times_table(int n)
+	
 {
 	int a = 0, rep, b;
 
