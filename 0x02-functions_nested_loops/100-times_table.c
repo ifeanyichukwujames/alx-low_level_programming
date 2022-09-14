c53edb39a228287d20ff6c017c4bacb1672d655d
“ifeanyichukwujames” <ifeanyichukwu.eze.james@gmail.com>
2022-09-14 06:22:29 -0700

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * prints_times_table - prints times table
- * @n : times table to use
- * Return: void
+ * print_times_table - prints times table
+ * @n: times table to use
+ * Return:void
  */
 
-void print_times_table(int n)
-	
+void print_times_table(int n)	
 {
 	int a = 0, rep, b;
 
