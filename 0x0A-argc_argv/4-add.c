30d0317b6c5a06e077eac31664f6034dfcd981c9
“ifeanyichukwujames” <ifeanyichukwu.eze.james@gmail.com>
2022-10-04 03:08:51 -0700

    Trying out argc and argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,7 +6,7 @@
  *  @argc: The number of arguments passed to the program.
  *  @argv: An array of pointers to the arguments.
  *  Return: If one of the numbers contains symbols that are non-digits - 1.
- *  	Otherwise return - 0
+ *	Otherwise return - 0
  */
 
 int main(int argc, char *argv[])
