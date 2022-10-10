c58a19a71d5615b98570ad3d3a099f01d3a2604b
“ifeanyichukwujames” <ifeanyichukwu.eze.james@gmail.com>
2022-10-10 10:52:27 -0700

    Preprocessing is working out well

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,5 @@
-#ifndef ABS_H
-#define ABS_H
-#define ABS(x) ((x) < (0) ? (-x) : (x))
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+#define ABS(x) ((x) < 0 ? (-x) : (x))
 extern int make_iso_compilers_happy;
 #endif
