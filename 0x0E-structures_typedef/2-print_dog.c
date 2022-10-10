5babfe25c9b1d207a1c7312091ccdd60829da6a2
“ifeanyichukwujames” <ifeanyichukwu.eze.james@gmail.com>
2022-10-10 16:02:37 -0700

    commit for structures

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - Print a dog
+ * @d: The dog to print
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: ");
+		if (d->name)
+			printf("%s", d->name);
+		else
+			printf("(nil)");
+		printf("\n");
+		printf("Age: ");
+		if (d->age)
+			printf("%f\n", d->age);
+		else
+			printf("(nil)");
+		printf("Owner: ");
+		if (d->owner)
+			printf("%s", d->owner);
+		else
+			printf("(nil)");
+		printf("\n");
+	}
+}
